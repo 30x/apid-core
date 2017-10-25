cc668c101d6764b1de262f79cc21f64c79f4d1bf
Sundar Ramamoorthy <ramamoorthy@google.com>
2017-10-25 14:26:22 -0700

    Add util function

--- util/util.go
+++ util/util.go
@@ -14,9 +14,24 @@
 
 package util
 
+
 import (
 	"github.com/google/uuid"
+	"net/http"
+	"net/url"
+	"github.com/apid/apid-core"
 )
+
+const (
+	configfwdProxyURL	=   "configfwdProxyURL"
+	configfwdProxyUser	=   "configfwdProxyUser"
+	configfwdProxyPasswd	=   "configfwdProxyPasswd"
+	configfwdProxyProtocol  =   "configfwdProxyProtocol"
+	configfwdProxyPort      =   "configfwdProxyPort"
+)
+
+var config apid.ConfigService
+
 
 func IsValidUUID(id string) bool {
 	_, err := uuid.Parse(id)
@@ -26,3 +41,38 @@ func IsValidUUID(id string) bool {
 func GenerateUUID() string {
 	return uuid.New().String()
 }
+
+func Transport() *http.Transport {
+	var tr http.Transport
+	var pURL *url.URL
+	var err error
+	// Apigee Forward Proxy
+	fwdPrxy := config.GetString(configfwdProxyURL)
+	fwdPrxyPro := config.GetString(configfwdProxyProtocol)
+	fwdPrxyUser := config.GetString(configfwdProxyUser)
+	fwdPrxyPass := config.GetString(configfwdProxyPasswd)
+	fwdPrxyPort := config.GetString(configfwdProxyPort)
+
+	if fwdPrxy != "" && fwdPrxyPro != "" && fwdPrxyUser != "" && fwdPrxyPort != "" {
+		pURL, err = url.Parse(fwdPrxyPro + "//" + fwdPrxyUser + ":" + fwdPrxyPass + "@" + fwdPrxy + ":" + fwdPrxyPort)
+		if err != nil {
+			panic("Error parsing proxy URL")
+		}
+	} else if fwdPrxy != "" && fwdPrxyPro != "" && fwdPrxyPort != "" {
+		pURL, err = url.Parse(fwdPrxyPro + "//" + fwdPrxy + ":" + fwdPrxyPort)
+		if err != nil {
+			panic("Error parsing proxy URL")
+		}
+	}
+
+	if pURL != nil {
+		tr = http.Transport{
+			Proxy:           http.ProxyURL(pURL),
+		}
+	} else {
+		tr = http.Transport{
+		}
+	}
+	return &tr
+}
+
